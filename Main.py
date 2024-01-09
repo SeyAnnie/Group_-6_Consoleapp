d6a156e31ab88e96bcba98ca04ccb0733060f19a
sheworth <sherifatoniyide@gmail.com>
2024-01-09 18:33:31 +0100

    I defined the main function for covid19 API using the get_data function

--- Main.py
+++ Main.py
@@ -47,3 +47,21 @@ def get_data(date=None, iso=None):
     print("New Cases: ", data["confirmed_diff"])
     print("New Deaths: ", data["deaths_diff"])
     print("Total Recovered: ", data["recovered"])
+    
+    BASE_URL_2 = "https://covid-api.com/api/"
+ 
+#main function for covid API using the get_data function
+def covid19():
+    print(
+        """Welcome to the COVID-19 Tracker App
+        Please select from the following options:
+        1. Global Data
+        2. Country Data
+        3. See the list of countries
+        4. Exit
+        """
+    )
+    choice = input("Enter your choice: ")
+    if choice == "4":
+        exit()
+    elif choice == "3":
